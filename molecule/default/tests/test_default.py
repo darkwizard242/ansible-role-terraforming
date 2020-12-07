06fbc753d5756ea0e3095cedc3a9b030d333db80
darkwizard242 <am900820@gmail.com>
2020-12-07 11:14:53 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,31 +6,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_ruby_package_installed(host):
-    assert host.package("ruby-full").is_installed or \
-      host.package("ruby").is_installed
+PACKAGE_BINARY = '/usr/local/bin/terrraforming'
 
 
-def test_ruby_binary_exists(host):
-    host.file('/usr/bin/ruby').exists
+def test_terrraforming_binary_exists(host):
+    """
+    Tests if terrraforming binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
-def test_ruby_binary_file(host):
-    host.file('/usr/bin/ruby').is_file
+def test_terrraforming_binary_file(host):
+    """
+    Tests if terrraforming binary is file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
-def test_ruby_binary_which(host):
-    assert host.check_output('which ruby') == '/usr/bin/ruby'
-
-
-def test_terraforming_binary_exists(host):
-    host.file('/usr/local/bin/terraforming').exists
-
-
-def test_terraforming_binary_file(host):
-    host.file('/usr/local/bin/terraforming').is_file
-
-
-def test_terraforming_binary_which(host):
-    assert host.check_output('which terraforming') == \
-     '/usr/local/bin/terraforming'
+def test_terrraforming_binary_which(host):
+    """
+    Tests the output to confirm terrraforming's binary location.
+    """
+    assert host.check_output('which terrraforming') == PACKAGE_BINARY
