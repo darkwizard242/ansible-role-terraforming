273080fd2fb6d8d0f7cc09e4b128418010f7c514
darkwizard242 <am900820@gmail.com>
2020-12-07 11:25:28 -0500

    fix testinfra functions

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,25 +6,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-PACKAGE_BINARY = '/usr/local/bin/terrraforming'
+PACKAGE_BINARY = '/usr/local/bin/terraforming'
 
 
-def test_terrraforming_binary_exists(host):
+def test_terraforming_binary_exists(host):
     """
-    Tests if terrraforming binary exists.
+    Tests if terraforming binary exists.
     """
     assert host.file(PACKAGE_BINARY).exists
 
 
-def test_terrraforming_binary_file(host):
+def test_terraforming_binary_file(host):
     """
-    Tests if terrraforming binary is file type.
+    Tests if terraforming binary is file type.
     """
     assert host.file(PACKAGE_BINARY).is_file
 
 
-def test_terrraforming_binary_which(host):
+def test_terraforming_binary_which(host):
     """
-    Tests the output to confirm terrraforming's binary location.
+    Tests the output to confirm terraforming's binary location.
     """
-    assert host.check_output('which terrraforming') == PACKAGE_BINARY
+    assert host.check_output('which terraforming') == PACKAGE_BINARY
